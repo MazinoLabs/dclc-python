1bc66a303b6e09ef0d5ece230e94e9b495e86a5c
Jessica Aki <jessaki230@gmail.com>
2026-02-06 07:35:10 +0000

    Exercise 2: Add function to remove fourth element and append it to second and last positions

--- data-structures.py
+++ data-structures.py
@@ -11,3 +11,13 @@ def odd_even_list(list1, list2):
 
     return new_list
 
+#Excercise 2: Remove the fourth element and add it to the second position and at the end of the list
+def remove_fourth_append_second(arr):
+    x = arr[4]
+    print(f"Original List {arr}")
+    arr.remove(x)
+    print(f"List After removing element at index 4 {arr}")
+    arr.insert(2, x)
+    print(f"List after Adding element at index 2 {arr}")
+    arr.append(x)
+    print(f"List after Adding element at last  {arr}")
