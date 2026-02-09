d41a8c744cc7bbb7ab1225e23ebd33a808bf8304
MazinoLabs <jessaki230@gmail.com>
2026-02-09 03:44:22 +0100

    Ensured proper Type handling of functions

--- basic-python.py
+++ basic-python.py
@@ -1,5 +1,5 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
-def sum_or_product(a, b):
+def sum_or_product(a: int, b: int):
     return a*b if a*b <= 1000 else a+b
 
 
@@ -9,7 +9,7 @@ print(sum_or_product(num_1, num_2))
 
 
 # Exercise 2: Print the Sum of a Current Number and a Previous number
-def summation(num):
+def summation(num: int):
     prev_num = 0
     for x in range(num):
         print(f"Current Number{x}Previous Number {prev_num}  Sum:{x+prev_num}")
@@ -21,7 +21,7 @@ summation(user_num)
 
 
 # Exercise 3: Print characters present at an even index number
-def even_index(word):
+def even_index(word: str):
     for x in range(len(word)):
         if x % 2 == 0:
             print(word[x])
@@ -32,7 +32,7 @@ even_index(phrase)
 
 
 # Exercise 4: Remove first n characters from a string
-def remove_chars(word, n):
+def remove_chars(word: str, n: int):
     return word[n:]
 
 
@@ -41,8 +41,8 @@ num_char = int(input("How many character should be removed"))
 print(remove_chars(word, num_char))
 
 
-#Exercise 5: Check if the first and last numbers of a list are the same
-def check_if_same(arr):
+# Exercise 5: Check if the first and last numbers of a list are the same
+def check_if_same(arr: list):
     first = arr[0]
     last = arr[len(arr)-1]
     return True if first == last else False
@@ -53,7 +53,7 @@ print(check_if_same([45, 5, 33, 14, 45]))
 
 
 # Exercise 6: Display numbers divisible by 5
-def div_by_five_list(arr):
+def div_by_five_list(arr: list):
     for x in arr:
         if x % 5 == 0:
             print(x)
@@ -64,7 +64,7 @@ div_by_five_list([3, 25, 40, 44, 60])
 
 
 # Exercise 7: Find the number of occurrences of a substring in a string
-def count_occurence(phrase, find):
+def count_occurence(phrase: str, find: str):
     return f"{find} was found {phrase.count(find)} times"
 
 
@@ -72,7 +72,7 @@ print(count_occurence("Emma is good developer. Emma is a writer", "Emma"))
 
 
 # Exercise 8: Print the following pattern
-def num_pyramid(n):
+def num_pyramid(n: int):
     for x in range(n+1):
         print((str(x)+" ")*x)
 
@@ -81,7 +81,7 @@ num_pyramid(5)
 
 
 # Exercise 9: Check Palindrome Number
-def check_palidrome(num):
+def check_palidrome(num: int):
     number = str(num)
     rev_num = ""
     for x in range(len(number)-1, -1, -1):
@@ -93,8 +93,8 @@ print(check_palidrome(125))
 print(check_palidrome(121))
 
 
-#Exercise 10: Merge two lists using the following condition
-def merge_lists(arr1, arr2):
+# Exercise 10: Merge two lists using the following condition
+def merge_lists(arr1: list, arr2: list):
     new_arr = []
     for x in arr1:
         if x % 2 != 0:
@@ -112,7 +112,7 @@ print(merge_lists(list1, list2))
 
 
 # Exercise 11: Get each digit from a number in the reverse order.
-def rev_number(num):
+def rev_number(num: int):
     number = str(num)
     rev_num = ""
     for x in range(len(number)-1, -1, -1):
@@ -125,7 +125,7 @@ print(rev_number(12345))
 
 
 # Exercise 12: Calculate income tax
-def tax_calculation(income):
+def tax_calculation(income: int):
     tax = 0
     if income > 20000:
         tax = 10000*0.1 + (income-20000)*0.2
@@ -139,7 +139,7 @@ def tax_calculation(income):
 print(tax_calculation(15000))
 
 
-#Exercise 13: Print multiplication table from 1 to 10
+# Exercise 13: Print multiplication table from 1 to 10
 for x in range(1, 11):
     for y in range(1, 11):
         print(x*y, end=" ")
@@ -152,10 +152,8 @@ for x in range(5, -1, -1):
 
 
 # Exercise 15: Get an int value of base raises to the power of exponent
-def exponent(base, exp):
+def exponent(base: int, exp: int):
     return base**exp
 
 
 print(exponent(2, 5))
-
-
